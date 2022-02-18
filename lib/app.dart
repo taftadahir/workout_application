d161f38bf7fe9d0cb6d74b41b0b7bb50d865370f
taftadjani <taftadjani@gmail.com>
2022-02-18 21:35:17 +0100

    Configure Globals - v1
    **Create file *global.dart* in *constants***
    
    **Create a class *Global* refer to global setting**
    
    **Add static const variable *appName* : *Workout Application***
    
    **Add the name of the app in the *MaterialApp* in *app.dart***

--- lib/app.dart
+++ lib/app.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:workout_application/constants/global.dart';
 
 class App extends StatelessWidget {
   const App({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) => const MaterialApp(
+        title: Global.appName,
         home: Scaffold(),
       );
 }
