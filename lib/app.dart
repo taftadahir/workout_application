f054793286be3bfef18b34c002bef9a752c14646
taftadjani <taftadjani@gmail.com>
2022-02-18 23:02:00 +0100

    Getx Configuration - v1
    - Update the *lib > app.dart* to use *GetMaterialApp*

--- lib/app.dart
+++ lib/app.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:workout_application/constants/global.dart';
 
 class App extends StatelessWidget {
   const App({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) => const MaterialApp(
+  Widget build(BuildContext context) => const GetMaterialApp(
         title: Global.appName,
         home: Scaffold(),
       );
