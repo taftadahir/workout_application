3aad8e2972877ca3d28dcb3c91345f4ef72d04f8
taftadjani <taftadjani@gmail.com>
2022-02-19 00:19:38 +0100

    Storage Configuration - v1
    Make *main* function async
    
    Use *WidgetsFlutterBinding.ensureInitialized();* to make sure that the storage is getting initialized before *runApp* get executed
    
    Initialize the storage using *await GetStorage.init();*

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:get_storage/get_storage.dart';
 import 'package:workout_application/app.dart';
 
-void main() => runApp(const App());
+Future<void> main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await GetStorage.init();
+  runApp(const App());
+}
