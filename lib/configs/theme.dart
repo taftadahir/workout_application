7a5330c6ddc0497a19ce1f9bd971e2cd335e6b0d
taftadjani <taftadjani@gmail.com>
2022-02-19 05:25:42 +0100

    Configure Theme - v3
    Update these variables : dark and light
    - Add *scaffoldBackgroundColor*

--- lib/configs/theme.dart
+++ lib/configs/theme.dart
@@ -3,14 +3,16 @@ import 'package:workout_application/configs/color.dart';
 
 class Theme{
   static final light = ThemeData(
-    backgroundColor: darkPalette.backgroundColor,
-    primaryColor: darkPalette.primaryColor,
+    backgroundColor: lightPalette.backgroundColor,
+    scaffoldBackgroundColor: lightPalette.backgroundColor,
+    primaryColor: lightPalette.primaryColor,
     brightness: Brightness.light,
   );
 
   static final dark = ThemeData(
-    backgroundColor: lightPalette.backgroundColor,
-    primaryColor: lightPalette.primaryColor,
+    backgroundColor: darkPalette.backgroundColor,
+    scaffoldBackgroundColor: darkPalette.backgroundColor,
+    primaryColor: darkPalette.primaryColor,
     brightness: Brightness.dark,
   );
 }
