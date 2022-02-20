2c5b67375b3e637de309a78d750e8b90385f0ddb
taftadjani <taftadjani@gmail.com>
2022-02-20 19:56:05 +0100

    Create Dashboard Screen - v1
    Create file *dashboard_screen.dart* in *views > screens*
    
    Create a stateless widget *DashboardScreen*
    
    Create a private var *_screens* containing a list of widgets ( Container ) with differentes color representing the screens.
    
    Use *GetBuilder* in the build method using the *BottomBarController* state
    
    Return a *Scaffold* widget as a result of the *GetBuilder*
    
    Add an *IndexedStack* widget as body of the *Scaffold*
    - *children* take the *_screens* variable
    - *index* take *BottomBarController* -> selectedItem
    
    Add *bottomNavigationBar* and use *BottomBarComponent*

---
+++ lib/views/screens/dashboard_screen.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:workout_application/controllers/bottom_bar_controller.dart';
+import 'package:workout_application/views/components/bottom_bar_component.dart';
+
+class DashboardScreen extends StatelessWidget {
+  const DashboardScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    List<Widget> screens = [
+      Container(
+        color: Colors.redAccent,
+      ),
+      Container(
+        color: Colors.blueAccent,
+      ),
+      Container(
+        color: Colors.deepPurpleAccent,
+      ),
+    ];
+    return GetBuilder<BottomBarController>(
+      builder: (controller) => Scaffold(
+        bottomNavigationBar: const BottomBarComponent(),
+        body: IndexedStack(
+          children: screens,
+          index: controller.selectedItem,
+        ),
+      ),
+    );
+  }
+}
