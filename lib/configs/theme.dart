93729d2dd2d56fc88235e8f38e3fb6ca409c6e7c
taftadjani <taftadjani@gmail.com>
2022-02-21 06:17:59 +0100

    Configure Theme - v5
    AppBar default theme configuration
    - *appBarTheme* : AppBarTheme with these option
    -  - *backgroundColor*: (darkPalette, lightPalette).backgroundColor
    -  - *elevation*: 0.0
    -  - iconTheme : IconThemeData with these option ( appbar icon theme )
    -  -  - *color*: (darkPalette, lightPalette).textColor
    -  - *titleTextStyle* : TextStyle with these option ( appbar title style )
    -  -  - *color*: (darkPalette, lightPalette).textColor
    -  -  - *fontSize*: 24.0
    -  -  - *fontWeight*: FontWeight.normal
    
    TextButton style default theme configuration
    - *textButtonTheme* : TextButtonThemeData with these option
    -  - *style* : ButtonStyle with these options
    -  -  - *backgroundColor* : MaterialStateProperty.all( (darkPalette, lightPalette).backgroundLightColor )
    -  -  - *padding* : MaterialStateProperty.all containing *EdgeInsets.symmetric* with these option
    -  -  -  - *horizontal*: 16.0,
    -  -  - *foregroundColor* : *MaterialStateProperty.all* containg
    -  -  -  - (darkPalette, lightPalette).textColor
    -  -  - *shape* : *MaterialStateProperty.all* containing *RoundedRectangleBorder* with these options
    -  -  -  - *borderRadius* : BorderRadius.circular ( 8.0 )
    -  -  - *textStyle* : *MaterialStateProperty.all* containing const TextStyle with these options
    -  -  -  - *fontSize*: 16.0
    
    Refactoring the theme
    - Create a static, private getter *_textButtonThemeDataLight* containing the text button theme for the light theme
    - Create a static, private getter *_textButtonThemeDataDark* containing the text button theme for the dark theme
    - Create a static, private getter *_appBarThemeLight* containing the appbar theme for the light theme
    - Create a static, private getter *_appBarThemeDark* containing the appbar theme for the dark theme
    - Create a static, private getter *_bottomNavigationBarThemeDataLight* containing the bottom navigation bar theme for the light theme
    - Create a static, private getter *_bottomNavigationBarThemeDataDark* containing the bottom navigation bar theme for the dark theme
    - Use these variable in the default theme data

--- lib/configs/theme.dart
+++ lib/configs/theme.dart
@@ -1,26 +1,135 @@
 import 'package:flutter/material.dart';
 import 'package:workout_application/configs/color.dart';
 
-class Theme{
+class Theme {
+  // Text button theme
+  static TextButtonThemeData get _textButtonThemeDataLight =>
+      TextButtonThemeData(
+        style: ButtonStyle(
+          backgroundColor: MaterialStateProperty.all(
+            lightPalette.backgroundLightColor,
+          ),
+          padding: MaterialStateProperty.all(
+            const EdgeInsets.symmetric(
+              horizontal: 16.0,
+            ),
+          ),
+          foregroundColor: MaterialStateProperty.all(
+            lightPalette.textColor,
+          ),
+          shape: MaterialStateProperty.all(
+            RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(
+                8.0,
+              ),
+            ),
+          ),
+          textStyle: MaterialStateProperty.all(
+            const TextStyle(
+              fontSize: 16.0,
+            ),
+          ),
+        ),
+      );
+
+  static TextButtonThemeData get _textButtonThemeDataDark =>
+      TextButtonThemeData(
+        style: ButtonStyle(
+          backgroundColor: MaterialStateProperty.all(
+            darkPalette.backgroundLightColor,
+          ),
+          padding: MaterialStateProperty.all(
+            const EdgeInsets.symmetric(
+              horizontal: 16.0,
+            ),
+          ),
+          foregroundColor: MaterialStateProperty.all(
+            darkPalette.textColor,
+          ),
+          shape: MaterialStateProperty.all(
+            RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(
+                8.0,
+              ),
+            ),
+          ),
+          textStyle: MaterialStateProperty.all(
+            const TextStyle(
+              fontSize: 16.0,
+            ),
+          ),
+        ),
+      );
+
+  // App bar theme
+  static AppBarTheme get _appBarThemeLight => AppBarTheme(
+        backgroundColor: lightPalette.backgroundColor,
+        elevation: 0.0,
+        iconTheme: IconThemeData(
+          color: lightPalette.textColor,
+        ),
+        titleTextStyle: TextStyle(
+          color: lightPalette.textColor,
+          fontSize: 24.0,
+          fontWeight: FontWeight.normal,
+        ),
+      );
+
+  static AppBarTheme get _appBarThemeDark => AppBarTheme(
+        backgroundColor: darkPalette.backgroundColor,
+        elevation: 0.0,
+        iconTheme: IconThemeData(
+          color: darkPalette.textColor,
+        ),
+        titleTextStyle: TextStyle(
+          color: darkPalette.textColor,
+          fontSize: 24.0,
+          fontWeight: FontWeight.normal,
+        ),
+      );
+
+  // Bottom navigation bar
+  static BottomNavigationBarThemeData get _bottomNavigationBarThemeDataLight =>
+      BottomNavigationBarThemeData(
+        backgroundColor: lightPalette.backgroundColor,
+        elevation: 0.0,
+        showSelectedLabels: false,
+        showUnselectedLabels: false,
+        selectedIconTheme: IconThemeData(
+          color: lightPalette.primaryColor,
+          size: 32.0,
+        ),
+        unselectedIconTheme: IconThemeData(
+          color: lightPalette.greyColor,
+          size: 24.0,
+        ),
+      );
+
+  static BottomNavigationBarThemeData get _bottomNavigationBarThemeDataDark =>
+      BottomNavigationBarThemeData(
+        backgroundColor: darkPalette.backgroundColor,
+        elevation: 0.0,
+        showSelectedLabels: false,
+        showUnselectedLabels: false,
+        selectedIconTheme: IconThemeData(
+          color: darkPalette.primaryColor,
+          size: 32.0,
+        ),
+        unselectedIconTheme: IconThemeData(
+          color: darkPalette.greyColor,
+          size: 24.0,
+        ),
+      );
+
+  // Theme data
   static final light = ThemeData(
     backgroundColor: lightPalette.backgroundColor,
     scaffoldBackgroundColor: lightPalette.backgroundColor,
     primaryColor: lightPalette.primaryColor,
     brightness: Brightness.light,
-    bottomNavigationBarTheme: BottomNavigationBarThemeData(
-      backgroundColor: lightPalette.backgroundColor,
-      elevation: 0.0,
-      showSelectedLabels: false,
-      showUnselectedLabels: false,
-      selectedIconTheme: IconThemeData(
-        color: lightPalette.primaryColor,
-        size: 32.0,
-      ),
-      unselectedIconTheme:  IconThemeData(
-        color: lightPalette.greyColor,
-        size: 24.0,
-      ),
-    ),
+    bottomNavigationBarTheme: _bottomNavigationBarThemeDataLight,
+    appBarTheme: _appBarThemeLight,
+    textButtonTheme: _textButtonThemeDataLight,
   );
 
   static final dark = ThemeData(
@@ -28,19 +137,8 @@ class Theme{
     scaffoldBackgroundColor: darkPalette.backgroundColor,
     primaryColor: darkPalette.primaryColor,
     brightness: Brightness.dark,
-    bottomNavigationBarTheme: BottomNavigationBarThemeData(
-      backgroundColor: darkPalette.backgroundColor,
-      elevation: 0.0,
-      showSelectedLabels: false,
-      showUnselectedLabels: false,
-      selectedIconTheme: IconThemeData(
-        color: darkPalette.primaryColor,
-        size: 32.0,
-      ),
-      unselectedIconTheme:  IconThemeData(
-        color: darkPalette.greyColor,
-        size: 24.0,
-      ),
-    ),
+    bottomNavigationBarTheme: _bottomNavigationBarThemeDataDark,
+    appBarTheme: _appBarThemeDark,
+    textButtonTheme: _textButtonThemeDataDark,
   );
 }
