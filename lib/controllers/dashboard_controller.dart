c48c3ca76b4cae1b64ca18390173a327addba8aa
taftadjani <taftadjani@gmail.com>
2022-02-23 00:26:43 +0100

    Create a Dashboard Controller - v3
    Drawer state fields
    - *_xOffset* : double, default as 0,
    - *_yOffset* : double, default as 0,
    - *_scaleFactor* : double, default as 1
    - *_isDragging* : bool, default as false
    - *_isDrawerOpen* : bool, default false
    
    Add a function *openDrawer*
    - Update *_xOffset* to *width x 0.8*
    - Update *yOffset* to *0.1 x screenHeight*
    - Update *scaleFactor* to *0.8*
    - Update *isDrawerOpen* to *true*
    - call *update* from GetxController
    
    Add a function *closeDrawer*
    - Update *xOffset* to *0*
    - Update *yOffset* to *0*
    - Update *scaleFactor* to *1*
    - Update *isDrawerOpen* to *false*
    - call *update* from GetxController
    
    Make the *screenIndex* private
    
    Add fields getters and setters and make sure to use *update* mehod from *Get* in setters to notify listeners

--- lib/controllers/dashboard_controller.dart
+++ lib/controllers/dashboard_controller.dart
@@ -2,10 +2,78 @@ import 'package:get/get.dart';
 
 class DashboardController extends GetxController {
   // 1 -> Home screen
-  int screenIndex = 1;
+  int _screenIndex = 1;
 
+  // Drawer properties
+  double _xOffset = 0;
+  double _yOffset = 0;
+  double _scaleFactor = 1;
+  bool _isDragging = false;
+  bool _isDrawerOpen = false;
+
+  // Getters
+  int get screenIndex => _screenIndex;
+
+  double get xOffset => _xOffset;
+
+  double get yOffset => _yOffset;
+
+  double get scaleFactor => _scaleFactor;
+
+  bool get isDragging => _isDragging;
+
+  bool get isDrawerOpen => _isDrawerOpen;
+
+  // Setters
+  set screenIndex(int screenIndex) {
+    _screenIndex = screenIndex;
+    update();
+  }
+
+  set xOffset(double xOffset) {
+    _xOffset = xOffset;
+    update();
+  }
+
+  set scaleFactor(double scaleFactor) {
+    _scaleFactor = scaleFactor;
+    update();
+  }
+
+  set yOffset(double yOffset) {
+    _yOffset = yOffset;
+    update();
+  }
+
+  set isDragging(bool isDragging) {
+    _isDragging = isDragging;
+    update();
+  }
+
+  set isDrawerOpen(bool isDrawerOpen) {
+    _isDrawerOpen = isDrawerOpen;
+    update();
+  }
+
+  // Methods
   void updateScreenIndex(int index) {
-    screenIndex = index;
+    _screenIndex = index;
+    update();
+  }
+
+  void openDrawer() {
+    _xOffset = Get.width * 0.8;
+    _yOffset = Get.height * 0.1;
+    _scaleFactor = 0.8;
+    _isDrawerOpen = true;
+    update();
+  }
+
+  void closeDrawer() {
+    _xOffset = 0;
+    _yOffset = 0;
+    _scaleFactor = 1;
+    _isDrawerOpen = false;
     update();
   }
 }
