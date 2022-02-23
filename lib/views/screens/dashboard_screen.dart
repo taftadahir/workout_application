3be5b66a124a67ec636e0f1c8e2283efc6b4b4bd
taftadjani <taftadjani@gmail.com>
2022-02-23 01:00:03 +0100

    Create Dashboard Screen - v3
    Create a private function *_buildDashboard* that is responsible of building our Dashboard
    
    Make *DashboardScreen* build function return a *Stack* with two children *DrawerScreen* and *_buildDashboard*
    
    Extract the *_screens* from the build function to the *DashboardScreen* class and make it static and final
    
    Make the dashboard rounded when drawer is opened using *ClipRRect* and it's *borderRadius* property
    
    Make the dashboard not responding to gesture when drawer is open
    - Use AbsorbPointer and it's property *absorbing*
    
    To open the drawer, we need to move the *DashboardScreen*
    - Add an *AnimatedContainer* to make the move smooth.
    -  - Use a duration of 250 milliseconds
    -  - Use *transform* with *Matrix4.translationValues* and use the *DashboardController* fields ( xOffset and yOffset )
    -  -  - Use also the *scale* function with *DashboardController* fields ( scaleFactor )
    -  - Make the corner of the container rounded and add a box shadow when the drawer is open
    
    We need to click the Dashboard when drawer are open in order to close it.
    - Use *GestureDetector*
    - Use the onTap property and give it *DashboardController.closeDrawer*  function
    
    We also need to close the drawer ondrag
    - Add *onHorizontalDragStart* and *onHorizontalDragUpdate* to the *GestureDetector*
    - *onHorizontalDragStart* : in this, we need to update the *controller.isDragging* to *true* so we can either *openDrawer* or *closeDrawer*
    
    We also need to close the drawer when we click the device back button
    - Use *WillPopScope* and it's *onWillPop* method
    
    Wrap the whole *Dashboard* with a *GetBuilder* of *DashboardController*
    
    Add the *switch button* in the home screen

--- lib/views/screens/dashboard_screen.dart
+++ lib/views/screens/dashboard_screen.dart
@@ -1,34 +1,122 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:workout_application/controllers/dashboard_controller.dart';
+import 'package:workout_application/services/theme_service.dart';
 import 'package:workout_application/views/components/app_bar_component.dart';
 import 'package:workout_application/views/components/bottom_bar_component.dart';
+import 'package:workout_application/views/screens/drawer_screen.dart';
 
 class DashboardScreen extends StatelessWidget {
   const DashboardScreen({Key? key}) : super(key: key);
 
-  @override
-  Widget build(BuildContext context) {
-    List<Widget> _screens = [
-      Container(
-        color: Colors.redAccent,
-      ),
-      Container(
-        color: Colors.blueAccent,
-      ),
-      Container(
-        color: Colors.deepPurpleAccent,
-      ),
-    ];
-    return GetBuilder<DashboardController>(
-      builder: (controller) => Scaffold(
-        appBar: AppBarComponent.getDashboardAppBar(index: controller.screenIndex),
-        bottomNavigationBar: const BottomBarComponent(),
-        body: IndexedStack(
-          children: _screens,
-          index: controller.screenIndex,
+  static final List<Widget> _screens = [
+    Container(
+      color: Colors.redAccent,
+    ),
+    Container(
+      color: Colors.blueAccent,
+      child: Center(
+        child: TextButton(
+          style: ButtonStyle(
+            backgroundColor: MaterialStateProperty.all(Colors.black45),
+            padding: MaterialStateProperty.all(
+              const EdgeInsets.symmetric(
+                horizontal: 24.0,
+                vertical: 8.0,
+              ),
+            ),
+            foregroundColor: MaterialStateProperty.all(Colors.white),
+          ),
+          onPressed: () => ThemeService.switchTheme(),
+          child: const Text('Switch Theme'),
         ),
       ),
+    ),
+    Container(
+      color: Colors.deepPurpleAccent,
+    ),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        const DrawerScreen(),
+        _buildDashboard(),
+      ],
     );
+  }
+
+  GetBuilder _buildDashboard() {
+    return GetBuilder<DashboardController>(builder: (controller) {
+      return WillPopScope(
+        onWillPop: () async {
+          if (controller.isDrawerOpen) {
+            controller.closeDrawer();
+            return false;
+          } else {
+            return true;
+          }
+        },
+        child: GestureDetector(
+          onTap: controller.closeDrawer,
+          onHorizontalDragStart: (details) => controller.isDragging = true,
+          onHorizontalDragUpdate: (details) {
+            if (!controller.isDragging) return;
+            const delta = 1;
+            if (details.delta.dx > delta) {
+              controller.openDrawer();
+            } else if (details.delta.dx < -delta) {
+              controller.closeDrawer();
+            }
+            controller.isDragging = false;
+          },
+          child: AnimatedContainer(
+            transform: Matrix4.translationValues(
+              controller.xOffset,
+              controller.yOffset,
+              0,
+            )..scale(controller.scaleFactor),
+            duration: const Duration(
+              milliseconds: 250,
+            ),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(
+                controller.isDrawerOpen ? 24 : 0,
+              ),
+              boxShadow: controller.isDrawerOpen
+                  ? [
+                BoxShadow(
+                  color:
+                  Get.theme.drawerTheme.scrimColor ?? Colors.white24,
+                  offset: const Offset(-8, 0),
+                  spreadRadius: 8,
+                  blurRadius: 16,
+                )
+              ]
+                  : [],
+            ),
+            child: AbsorbPointer(
+              absorbing: controller.isDrawerOpen,
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(
+                  controller.isDrawerOpen ? 24 : 0,
+                ),
+                child: Scaffold(
+                  appBar: AppBarComponent.getDashboardAppBar(
+                    index: controller.screenIndex,
+                  ),
+                  bottomNavigationBar: const BottomBarComponent(),
+                  body: IndexedStack(
+                    children: _screens,
+                    index: controller.screenIndex,
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ),
+      );
+    });
   }
 }
