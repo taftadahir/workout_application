7294b37fe94675de1dd3c7d62e56901270cdde4f
taftadjani <taftadjani@gmail.com>
2022-02-25 00:34:40 +0100

    Getx Configuration - v3
    Bind the *OnboardingController* in *dependencies* of AppBinding
    - Use *lazyPut* from *Get* package

--- lib/bindings/app_binding.dart
+++ lib/bindings/app_binding.dart
@@ -1,10 +1,12 @@
 import 'package:get/get.dart';
 import 'package:workout_application/controllers/dashboard_controller.dart';
+import 'package:workout_application/controllers/onboarding_controller.dart';
 
 class AppBinding extends Bindings {
   @override
   void dependencies() {
     // Implement dependencies
     Get.lazyPut(() => DashboardController());
+    Get.lazyPut(() => OnboardingController());
   }
 }
