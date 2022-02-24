4fe5ab80d8949fc5c43dbc99e0b3183bc0fab0f2
taftadjani <taftadjani@gmail.com>
2022-02-25 00:23:51 +0100

    Create Onboarding Component - v1
    Create file *onboarding_component.dart* in *views > components*
    
    Create a stateless widget *OnboardingComponent*
    
    Add Properties and constructors
    
    Make sure the width of the onboarding screen take the whole width
    
    Add the image part using *SvgPicture.asset* with
    - *width* = *Get.width*
    - Take the whole remaining height: use *Expanded*
    
    Spacing
    - *40px* - between *image* and *title*
    - *16px* - between *title* and *description*
    
    Add the description
    
    Add the title

---
+++ lib/views/components/onboarding_component.dart
@@ -0,0 +1,50 @@
+
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:get/get.dart';
+import 'package:workout_application/configs/layout.dart';
+import 'package:workout_application/models/onboarding.dart';
+
+class OnboardingComponent extends StatelessWidget {
+  final Onboarding onboarding;
+
+  const OnboardingComponent({
+    Key? key,
+    required this.onboarding,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: Get.width > Layout.maxWidth ? Layout.maxWidth : Get.width,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Expanded(
+            child: SvgPicture.asset(
+              'assets/images/${onboarding.image}',
+              width: Get.width,
+            ),
+          ),
+          const SizedBox(height: 40),
+          Text(
+            onboarding.title,
+            style: const TextStyle(
+              fontSize: 32,
+              fontWeight: FontWeight.w800,
+            ),
+          ),
+          const SizedBox(height: 16),
+          Text(
+            onboarding.description,
+            textAlign: TextAlign.center,
+            style: const TextStyle(
+              fontSize: 16,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
