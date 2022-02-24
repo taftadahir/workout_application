e7b2644d0fb999bf9c50aceb2612fcffd234f38d
taftadjani <taftadjani@gmail.com>
2022-02-25 00:16:44 +0100

    Create Onboarding Model - v1
    Create file *onboarding.dart* in *models*
    
    Create a class *Onboarding*
    
    Add properties and constructors

---
+++ lib/models/onboarding.dart
@@ -0,0 +1,11 @@
+class Onboarding{
+  String image;
+  String title;
+  String description;
+
+  Onboarding({
+    required this.image,
+    required this.title,
+    required this.description,
+  });
+}
