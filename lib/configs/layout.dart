abbcaecd7ec7870f482cfa4dab40f96932ebe0a4
taftadjani <taftadjani@gmail.com>
2022-02-24 23:49:53 +0100

    Configure Layouts - v1
    Create file *layout.dart* in *configs*
    
    Create a class *Layout*
    
     Add *variables* as static, const

---
+++ lib/configs/layout.dart
@@ -0,0 +1,13 @@
+class Layout {
+  // Height
+  static const double minHeight = 568.0;
+  static const double maxHeight = 1366.0;
+
+  // Width
+  static const double minWidth = 320.0;
+  static const double maxWidth = 720.0;
+
+  // Screen Padding
+  static const double horizontalScreenPadding = 16.0;
+  static const double verticalScreenPadding = 16.0;
+}
