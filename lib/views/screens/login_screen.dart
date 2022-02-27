1cd0fb33ad43f4f02a2f8f16a361a88edd9a088a
taftadjani <taftadjani@gmail.com>
2022-02-27 06:51:23 +0100

    Create Login Screen - v1
    Create *login_screen.dart* file in *views > screens*
    
    Create a class called *LoginScreen* extending *GetView* of type *LoginController*
    
    Screen height take at least the screen height
    - As we use *SafeArea*, we should substract with the *top* and *bottom* of the screen
    
    Create *_screenContent* getter and get the screen code
    - Containing a list of widgets used in *children* of *Column*
    
    Use a *SingleChildScrollView* with a *Form* as it's child which also take a *ConstrainedBox* > *Column*
    
    Add the *form key*( *loginFormKey* ) from the controller *LoginController*
    
    Email field configuration
    - Add *emailController* from *LoginController*
    - *onSaved* update the *email* variable from controller. This function ( from *onSaved* ) is executed when *loginFormKey.currentState!.save()*
    - Use *FieldContainerComponent* and *EmailFieldComponent*
    
    Password field configuration
    - Use *passwordController* from *LoginController*
    - *onSaved* update the *password* variable from controller. This function ( from *onSaved* ) is executed when *loginFormKey.currentState!.save()*
    - Use *FieldContainerComponent* and *PasswordFieldComponent*
    
    Form configuration
    - *autovalidateMode* : use *onUserInteraction* which update the field each time user interact => if error were shown, it will be gone
    
    Layout configuration ( Use *ConstrainedBox* )
    - Keyboard closed
    -  - take the whole screen if screen too big, else scroll if necessary for smaller screen height
    - Keyboard opened
    -  - Reduce spacing between element so if the screen can take the (keyboard + screen content), then it take whitout scroll; else use scroll
    - Issue found, when I used *Get.mediaQuery.viewInsets.bottom*, the screen doesn't update when we close the keyboard, I resolve it using the default *MediaQuery.of(context).viewInsets.bottom*
    
    Adding logo
    - Need to specify the *minHeight* = 200
    - *maxHeight* = 320
    - *maxWidth* = *Get.width*
    - Use *SvgPicture* and *Container* with *constraints*
    
    Spacing
    - *40px* between *topbar* and *logo*
    - *40px* between *logo* and *Title*
    - *40px* between *title* and *EmailField*
    - *8px* between *EmailField* and *PasswordField*
    - *24px* between *PasswordField* and *RegisterButton*
    - *24px* between *RegisterButton* and the *bottom*
    
    Add title
    - Use *TitleComponent*
    
    Add *IconButton* to redirect to *HomeScreen*
    - Clear the email and password when we click it
    - Use *Get.offNamed* -> This delete the *LoginController* dependency from memory

---
+++ lib/views/screens/login_screen.dart
@@ -0,0 +1,126 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:get/get.dart';
+import 'package:workout_application/configs/behavior.dart';
+import 'package:workout_application/configs/layout.dart';
+import 'package:workout_application/configs/routes.dart';
+import 'package:workout_application/controllers/login_controller.dart';
+import 'package:workout_application/views/components/button_component.dart';
+import 'package:workout_application/views/components/email_field_component.dart';
+import 'package:workout_application/views/components/field_container_component.dart';
+import 'package:workout_application/views/components/password_field_component.dart';
+import 'package:workout_application/views/components/title_component.dart';
+
+class LoginScreen extends GetView<LoginController> {
+  const LoginScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: SingleChildScrollView(
+          physics: Behavior.physics,
+          child: Form(
+            key: controller.loginFormKey,
+            autovalidateMode: AutovalidateMode.onUserInteraction,
+            child: ConstrainedBox(
+              constraints: BoxConstraints(
+                minHeight: Get.height -
+                    MediaQuery.of(context).viewInsets.bottom -
+                    Get.mediaQuery.padding.top -
+                    Get.mediaQuery.padding.bottom,
+              ),
+              child: Padding(
+                padding: const EdgeInsets.symmetric(
+                  horizontal: Layout.horizontalScreenPadding,
+                  vertical: Layout.verticalScreenPadding,
+                ),
+                child: Column(
+                  mainAxisSize: MainAxisSize.max,
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: _screenContent,
+                ),
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  List<Widget> get _screenContent {
+    return [
+      Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          IconButton(
+            onPressed: () {
+              Get.offNamed(AppRoute.dashboardScreen);
+            },
+            icon: const Icon(Icons.home_max_rounded),
+          ),
+          TextButton(
+            onPressed: () {},
+            child: const Text('Register'),
+          ),
+        ],
+      ),
+      Container(
+        constraints: BoxConstraints(
+          maxHeight: 320,
+          minHeight: 200,
+          maxWidth: Get.width,
+        ),
+        margin: const EdgeInsets.only(top: 40.0),
+        child: SvgPicture.asset(
+          'assets/images/app_icon.svg',
+          // width: Get.width,
+        ),
+      ),
+      Column(
+        children: [
+          const SizedBox(
+            height: 40,
+          ),
+          const TitleComponent(text: 'Sign in'),
+          const SizedBox(
+            height: 40.0,
+          ),
+          FieldContainerComponent(
+            child: EmailFieldComponent(
+              controller: controller.emailController,
+              onSaved: (value) => controller.email = value!,
+            ),
+          ),
+          const SizedBox(
+            height: 8.0,
+          ),
+          FieldContainerComponent(
+            child: PasswordFieldComponent(
+              controller: controller.passwordController,
+              onSaved: (value) => controller.password = value!,
+            ),
+          ),
+          const SizedBox(
+            height: 24,
+          ),
+          ConstrainedBox(
+            constraints: const BoxConstraints(
+              minWidth: 240,
+              maxHeight: 56,
+              maxWidth: 320,
+              minHeight: 56,
+            ),
+            child: ButtonComponent(
+              text: 'Register',
+              onPressed: controller.login,
+            ),
+          ),
+          const SizedBox(
+            height: 8.0,
+          ),
+        ],
+      ),
+    ];
+  }
+}
