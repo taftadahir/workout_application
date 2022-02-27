8c41ded1c9f50920988b6a11703606636a5cb99d
taftadjani <taftadjani@gmail.com>
2022-02-27 06:08:24 +0100

    Configure Globals - v3
    Add static const variable *authTitle* : *auth_title*
    
    Add static const variable *fieldTitle* : *field_title*
    
    Add static const variable *emailValidation* : *Please, provide a valid email*
    
    Add static const variable *passwordValidation* : *Password contains at least 8 characters*

--- lib/constants/global_constant.dart
+++ lib/constants/global_constant.dart
@@ -1,3 +1,12 @@
-class GlobalConstant{
+class GlobalConstant {
   static const String appName = 'Workout Application';
+
+  // Title Type
+  static const String authTitle = 'auth_title';
+  static const String fieldTitle = 'field_title';
+
+  // Messages
+  static const String emailValidation = 'Please, provide a valid email';
+  static const String passwordValidation =
+      'Password contains at least 8 characters';
 }
