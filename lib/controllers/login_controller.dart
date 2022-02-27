141797282f09280c1e6c8aa5a5e0176ecdf84338
taftadjani <taftadjani@gmail.com>
2022-02-27 06:29:36 +0100

    Create Login Controller - v1
    Create file *login_controller.dart* in *controller*
    
    Create class *LoginController* extending *GetxController*
    
    Override the *onInit* and *onClose* methods
    - *onInit* method, initialize the controller
    - *close* method, dispose the controller
    
    Add *parameters*
    - Make sure they initialized in *onInit* method
    
    Create *login* method
    - Use the controller to get the values
    - Use the values to validate ( use *isEmail* from *Getx* ) and for the password, use only the length
    - If there is an error, show *snackBar* with all error message
    - If there is no error, save the form so *onSaved* properties of the *fields* get executed. Should

---
+++ lib/controllers/login_controller.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:workout_application/configs/behavior.dart';
+import 'package:workout_application/constants/global_constant.dart';
+
+class LoginController extends GetxController {
+  late TextEditingController emailController, passwordController;
+  late String email, password;
+  final GlobalKey<FormState> loginFormKey = GlobalKey<FormState>();
+
+  @override
+  void onInit() {
+    super.onInit();
+    emailController = TextEditingController();
+    passwordController = TextEditingController();
+    email = '';
+    password = '';
+  }
+
+  @override
+  void onClose() {
+    emailController.dispose();
+    passwordController.dispose();
+    super.onClose();
+  }
+
+  void login() {
+    String message = '';
+    bool hasError = false;
+    if (!emailController.text.isEmail) {
+      message += GlobalConstant.emailValidation;
+      hasError = true;
+    }
+
+    if (passwordController.text.length < 8) {
+      if (message == '') {
+        message += GlobalConstant.passwordValidation;
+      } else {
+        message += '\n${GlobalConstant.passwordValidation}';
+      }
+      hasError = true;
+    }
+    
+    if (hasError) {
+      Get.showSnackbar(
+        GetSnackBar(
+          title: 'Fields validation',
+          message: message,
+          snackPosition: SnackPosition.TOP,
+          dismissDirection: DismissDirection.vertical,
+          backgroundColor: Get.theme.snackBarTheme.backgroundColor ??
+              const Color(0xFF303030),
+          duration: Behavior.snackDuration,
+          snackStyle: SnackStyle.GROUNDED,
+          animationDuration: Behavior.animationDuration,
+        ),
+      );
+    } else {
+      loginFormKey.currentState!.save();
+
+      // Here we make the request to login
+    }
+  }
+}
