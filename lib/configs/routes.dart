8196b2195093528aae84c497f0ab22e9c06a249f
taftadjani <taftadjani@gmail.com>
2022-03-01 00:28:32 +0100

    Configure Application Routing - v4
    - [x] t12 - Add a *static*, *const*, *String* variable *registerScreen* containing */register_screen* in *routes.dart*
    - [x] t13 - Add a new page in *_getPages* variable which redirect to

--- lib/configs/routes.dart
+++ lib/configs/routes.dart
@@ -2,24 +2,30 @@ import 'package:get/get.dart';
 import 'package:workout_application/views/screens/dashboard_screen.dart';
 import 'package:workout_application/views/screens/login_screen.dart';
 import 'package:workout_application/views/screens/onboarding_screen.dart';
+import 'package:workout_application/views/screens/register_screen.dart';
 
 class AppRoute {
   static const String dashboardScreen = '/dashboard_screen';
   static const String onboardingScreen = '/onboarding_screen';
   static const String loginScreen = '/login_screen';
+  static const String registerScreen = '/register_screen';
 
   static List<GetPage<dynamic>> get getPages => [
-    GetPage(
-      name: AppRoute.dashboardScreen,
-      page: () => const DashboardScreen(),
-    ),
-    GetPage(
-      name: AppRoute.onboardingScreen,
-      page: () => const OnboardingScreen(),
-    ),
-    GetPage(
-      name: AppRoute.loginScreen,
-      page: () => const LoginScreen(),
-    ),
-  ];
+        GetPage(
+          name: AppRoute.dashboardScreen,
+          page: () => const DashboardScreen(),
+        ),
+        GetPage(
+          name: AppRoute.onboardingScreen,
+          page: () => const OnboardingScreen(),
+        ),
+        GetPage(
+          name: AppRoute.loginScreen,
+          page: () => const LoginScreen(),
+        ),
+        GetPage(
+          name: AppRoute.registerScreen,
+          page: () => const RegisterScreen(),
+        ),
+      ];
 }
