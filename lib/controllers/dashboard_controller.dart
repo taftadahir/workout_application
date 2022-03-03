e844f7c93f8d5fce95c1db978ee2b52394e54253
taftadjani <taftadjani@gmail.com>
2022-03-03 01:47:45 +0100

    Create a Dashboard Controller - v4
    - [x] t14 - Create getters responsible of getting the programs and the exercises
    	- [x] *popularPrograms*
    	- [x] *calisthenicPrograms*
    	- [x] *bodyBuildPrograms*
    	- [x] *homeWorkoutPrograms*
    	- [x] *boxingPrograms*
    	- [x] *popularExercises*

--- lib/controllers/dashboard_controller.dart
+++ lib/controllers/dashboard_controller.dart
@@ -1,4 +1,5 @@
 import 'package:get/get.dart';
+import 'package:workout_application/models/program.dart';
 
 class DashboardController extends GetxController {
   // 1 -> Home screen
@@ -76,4 +77,172 @@ class DashboardController extends GetxController {
     _isDrawerOpen = false;
     update();
   }
+
+  // Get Popular Programs
+  static List<Program> get popularPrograms => [
+        Program(
+            id: 1,
+            userId: 1,
+            sysId: 'Program1',
+            name: 'Leg workout',
+            published: true,
+            days: 23,
+            image: 'v_sit.png'),
+        Program(
+            id: 1,
+            userId: 1,
+            sysId: 'Program1',
+            name: 'Leg workout',
+            published: true,
+            days: 23,
+            image: 'handstand.png'),
+        Program(
+            id: 1,
+            userId: 1,
+            sysId: 'Program1',
+            name: 'Leg workout',
+            published: true,
+            days: 23,
+            image: 'v_sit.png'),
+      ];
+
+  // Get Calisthenic Programs
+  static List<Program> get calisthenicPrograms => [
+        Program(
+            id: 1,
+            userId: 1,
+            sysId: 'Program1',
+            name: 'Leg workout',
+            published: true,
+            days: 23,
+            image: 'v_sit.png'),
+        Program(
+            id: 1,
+            userId: 1,
+            sysId: 'Program1',
+            name: 'Leg workout',
+            published: true,
+            days: 23,
+            image: 'handstand.png'),
+        Program(
+            id: 1,
+            userId: 1,
+            sysId: 'Program1',
+            name: 'Leg workout',
+            published: true,
+            days: 23,
+            image: 'v_sit.png'),
+      ];
+
+  // Get BodyBuild Programs
+  static List<Program> get bodyBuildPrograms => [
+        Program(
+            id: 1,
+            userId: 1,
+            sysId: 'Program1',
+            name: 'Leg workout',
+            published: true,
+            days: 23,
+            image: 'v_sit.png'),
+        Program(
+            id: 1,
+            userId: 1,
+            sysId: 'Program1',
+            name: 'Leg workout',
+            published: true,
+            days: 23,
+            image: 'handstand.png'),
+        Program(
+            id: 1,
+            userId: 1,
+            sysId: 'Program1',
+            name: 'Leg workout',
+            published: true,
+            days: 23,
+            image: 'v_sit.png'),
+      ];
+
+  // Get Home Workout Programs
+  static List<Program> get homeWorkoutPrograms => [
+        Program(
+            id: 1,
+            userId: 1,
+            sysId: 'Program1',
+            name: 'Leg workout',
+            published: true,
+            days: 23,
+            image: 'v_sit.png'),
+        Program(
+            id: 1,
+            userId: 1,
+            sysId: 'Program1',
+            name: 'Leg workout',
+            published: true,
+            days: 23,
+            image: 'handstand.png'),
+        Program(
+            id: 1,
+            userId: 1,
+            sysId: 'Program1',
+            name: 'Leg workout',
+            published: true,
+            days: 23,
+            image: 'v_sit.png'),
+      ];
+
+  // Get Boxing Programs
+  static List<Program> get boxingPrograms => [
+        Program(
+            id: 1,
+            userId: 1,
+            sysId: 'Program1',
+            name: 'Leg workout',
+            published: true,
+            days: 23,
+            image: 'v_sit.png'),
+        Program(
+            id: 1,
+            userId: 1,
+            sysId: 'Program1',
+            name: 'Leg workout',
+            published: true,
+            days: 23,
+            image: 'handstand.png'),
+        Program(
+            id: 1,
+            userId: 1,
+            sysId: 'Program1',
+            name: 'Leg workout',
+            published: true,
+            days: 23,
+            image: 'v_sit.png'),
+      ];
+
+  // Popular Exercises
+  static List<Program> get popularExercises => [
+        Program(
+            id: 1,
+            userId: 1,
+            sysId: 'Program1',
+            name: 'Leg workout',
+            published: true,
+            days: 23,
+            image: 'v_sit.png'),
+        Program(
+            id: 1,
+            userId: 1,
+            sysId: 'Program1',
+            name: 'Leg workout',
+            published: true,
+            days: 23,
+            image: 'handstand.png'),
+        Program(
+            id: 1,
+            userId: 1,
+            sysId: 'Program1',
+            name: 'Leg workout',
+            published: true,
+            days: 23,
+            image: 'v_sit.png'),
+      ];
 }
