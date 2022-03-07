95d4b2b0917ff7ca36ae0572f9f138808df9ef85
taftadjani <taftadjani@gmail.com>
2022-03-07 02:07:37 +0100

    Getx Configuration - v5
    - [x] t8 - Add 2 new dependencies for *RoutineController* and *HomeController*

--- lib/bindings/app_binding.dart
+++ lib/bindings/app_binding.dart
@@ -1,8 +1,10 @@
 import 'package:get/get.dart';
 import 'package:workout_application/controllers/dashboard_controller.dart';
+import 'package:workout_application/controllers/home_controller.dart';
 import 'package:workout_application/controllers/login_controller.dart';
 import 'package:workout_application/controllers/onboarding_controller.dart';
 import 'package:workout_application/controllers/register_controller.dart';
+import 'package:workout_application/controllers/routine_controller.dart';
 
 class AppBinding extends Bindings {
 
@@ -15,5 +17,7 @@ class AppBinding extends Bindings {
     Get.lazyPut(() => OnboardingController(), fenix: true);
     Get.lazyPut(() => LoginController(), fenix: true);
     Get.lazyPut(() => RegisterController(), fenix: true);
+    Get.lazyPut(() => RoutineController(), fenix: true);
+    Get.lazyPut(() => HomeController(), fenix: true);
   }
 }
