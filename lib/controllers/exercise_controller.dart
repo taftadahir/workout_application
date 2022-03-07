feffd7fd5c09f1222fcb345d700901a3b2fdcdad
taftadjani <taftadjani@gmail.com>
2022-03-07 01:44:08 +0100

    Create Exercise Controller - v1
    - [x] t1 - Create file *exercise_controller.dart* in *controllers*
    - [x] t2 - Create a class *ExerciseController*
    - [x] t3 - Create a method *getExerciseById* which use the exercise id to get the exercise

---
+++ lib/controllers/exercise_controller.dart
@@ -0,0 +1,11 @@
+import 'package:workout_application/models/exercise.dart';
+
+class ExerciseController {
+  static Exercise? getExerciseById({required int exerciseId}) => Exercise(
+        id: exerciseId,
+        userId: 1,
+        sysId: 'Exercise0',
+        name: 'Push Up',
+        published: true,
+      );
+}
