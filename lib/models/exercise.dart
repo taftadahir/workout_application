a7531888f9a88152efe0df6f729a9e72cdcce724
taftadjani <taftadjani@gmail.com>
2022-03-07 01:42:51 +0100

    Create Exercise Model - v1
    - [x] t1 - Create file *exercise.dart* in *models*
    - [x] t2 - Create a class *Exercise*
    - [x] t3 - Add properties and constructors

---
+++ lib/models/exercise.dart
@@ -0,0 +1,29 @@
+class Exercise {
+  final int id;
+  final int userId;
+  final String sysId;
+  final String name;
+  final bool published;
+  final bool? timeBased;
+  final bool? repBased;
+  final bool? useWeight;
+  final String? description;
+  final DateTime? createdAt;
+  final DateTime? updatedAt;
+  final DateTime? deletedAt;
+
+  Exercise({
+    required this.id,
+    required this.userId,
+    required this.sysId,
+    required this.name,
+    required this.published,
+    this.timeBased,
+    this.repBased,
+    this.useWeight,
+    this.description,
+    this.createdAt,
+    this.updatedAt,
+    this.deletedAt,
+  });
+}
