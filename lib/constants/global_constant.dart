4ef13af13d7cc3fb52f20f1e69b15005da494d33
taftadjani <taftadjani@gmail.com>
2022-03-08 23:15:12 +0100

    Configure Globals - v8
    - [x] t20 - Add *exerciseViewTitle* var with value *exercise_view_title*

--- lib/constants/global_constant.dart
+++ lib/constants/global_constant.dart
@@ -9,6 +9,7 @@ class GlobalConstant {
   static const String workoutTitle = 'workout_title';
   static const String calendarMonth = 'calendar_month';
   static const String exerciseCardTitle = 'exercise_card_title';
+  static const String exerciseViewTitle = 'exercise_view_title';
 
   // Messages
   static const String emailValidation = 'Please, provide a valid email';
