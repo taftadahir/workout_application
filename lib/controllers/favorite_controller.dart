3eff633678e7eb12755d013280d40e2899125d0c
taftadjani <taftadjani@gmail.com>
2022-03-10 01:32:35 +0100

    Create Favorite Controller - v3
    - [x] t7 - Add *_names* and *_images* and use it in order to see different images and names
    	- [x] Use these var

--- lib/controllers/favorite_controller.dart
+++ lib/controllers/favorite_controller.dart
@@ -7,16 +7,40 @@ class FavoriteController extends GetxController {
   @override
   void onInit() {
     super.onInit();
-    // Initialize the _exercises variable
+    List<String> _images = [
+      'back_lever.png',
+      'front_lever.png',
+      'handstand.png',
+      'handstand_push_up.png',
+      'human_flag.png',
+      'muscle_up.png',
+      'one_arm_front_lever.png',
+      'pistol_squat.png',
+      'planche.png',
+      'v_sit.png',
+    ];
+
+    List<String> _names = [
+      'Back Lever',
+      'Front Lever',
+      'Handstand',
+      'Handstand Push Up',
+      'Human Flag',
+      'Muscle Up',
+      'One Arm Front Lever',
+      'Pistol Squat',
+      'Planche',
+      'V sit',
+    ]; // Initialize the _exercises variable
     _exercises = List.generate(
       10,
       (index) => Exercise(
         id: index,
         userId: 1,
         sysId: 'Exercise$index',
-        name: 'Push Up - $index',
+        name: _names[index],
         published: true,
-        image: 'back_lever.png',
+        image: _images[index],
         description: index % 2 == 0
             ? 'In the previous post, I wrote about what the box '
                 'constraints are and how they work at a very basic level. In this'
