c70f4ddba7c9ad69e194eab276158dca37290d46
taftadjani <taftadjani@gmail.com>
2022-03-12 04:22:36 +0100

    Create Counter Controller - v2
    - [x] t8 - Make *_count* public
    - [x] t9 - Remove getter and setter

--- lib/controllers/counter_controller.dart
+++ lib/controllers/counter_controller.dart
@@ -3,13 +3,13 @@ import 'dart:async';
 import 'package:get/get.dart';
 
 class CounterController extends GetxController {
-  int _count = 0;
+  int count = 0;
   late Timer _timer;
 
   void start() {
     _timer = Timer.periodic(const Duration(seconds: 1), (timer) {
-      if (_count > 0) {
-        _count--;
+      if (count > 0) {
+        count--;
       } else {
         _timer.cancel();
       }
@@ -24,13 +24,6 @@ class CounterController extends GetxController {
 
   void reset() {
     _timer.cancel();
-    _count = 0;
+    count = 0;
   }
-
-  set count(int count) {
-    _count = count;
-    update();
-  }
-
-  int get count => _count;
 }
