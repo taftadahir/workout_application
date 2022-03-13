33cd152c3e990671fa2a8b56e8319e2d0a486008
taftadjani <taftadjani@gmail.com>
2022-03-13 05:03:47 +0100

    Create Program Controller - v2
    - [x] t4 - Create a method *getProgramWorkouts* which use the program id to get the workouts

--- lib/controllers/program_controller.dart
+++ lib/controllers/program_controller.dart
@@ -1,4 +1,5 @@
 import 'package:workout_application/models/program.dart';
+import 'package:workout_application/models/workout.dart';
 
 class ProgramController {
   static Program? getProgramById({required int programId}) => Program(
@@ -7,5 +8,23 @@ class ProgramController {
         sysId: 'Program0',
         name: 'Leg Workout',
         published: true,
+      );
+
+  static List<Workout> getProgramWorkouts({required int programId}) =>
+      List.generate(
+        10,
+        (index) => Workout(
+          id: 1,
+          exerciseId: 1,
+          programId: programId,
+          useWeight: true,
+          weight: 70,
+          timeBased: false,
+          time: 45,
+          repBased: true,
+          reps: 7,
+          set: 3,
+          restTime: 180,
+        ),
       );
 }
