6e0e756bdba0aeafa5a8c88e5f1effaa8a7098f3
taftadjani <taftadjani@gmail.com>
2022-03-13 05:46:14 +0100

    Create Program Day View Screen - v1
    - [x] t1 - Create file *program_day_view_screen.dart* in *views > screens*
    - [x] t2 - Create a stateless widget *ProgramDayViewScreen*
    - [x] t3 - Add image
    - [x] t4 - Add list of workouts

---
+++ lib/views/screens/program_day_view_screen.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:workout_application/configs/behavior.dart';
+import 'package:workout_application/configs/layout.dart';
+import 'package:workout_application/controllers/program_controller.dart';
+import 'package:workout_application/models/program.dart';
+import 'package:workout_application/models/workout.dart';
+import 'package:workout_application/views/components/app_bar_component.dart';
+import 'package:workout_application/views/components/workout_card_component.dart';
+
+class ProgramDayViewScreen extends StatelessWidget {
+  final Program program;
+  final int day;
+
+  const ProgramDayViewScreen(
+      {Key? key, required this.program, required this.day})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    List<Workout> _workouts = ProgramController.getProgramWorkouts(
+      programId: program.id,
+    );
+
+    return Scaffold(
+      appBar: AppBarComponent.getAppBar(
+        title: Text(program.name),
+        leading: IconButton(
+          icon: const Icon(Icons.arrow_back_ios_rounded),
+          onPressed: () => Get.back(),
+        ),
+      ),
+      body: SafeArea(
+        child: ListView(
+          physics: Behavior.physics,
+          padding: const EdgeInsets.symmetric(
+            horizontal: Layout.horizontalScreenPadding,
+            vertical: Layout.horizontalScreenPadding,
+          ),
+          children: [
+            Image.asset(
+              'assets/images/${program.image}',
+              fit: BoxFit.cover,
+              width: Get.width,
+            ),
+            const SizedBox(
+              height: 24,
+            ),
+            ..._workouts
+                .map(
+                  (workout) => Padding(
+                    padding: const EdgeInsets.only(bottom: 16),
+                    child: WorkoutCardComponent(workout: workout),
+                  ),
+                )
+                .toList(),
+          ],
+        ),
+      ),
+    );
+  }
+}
