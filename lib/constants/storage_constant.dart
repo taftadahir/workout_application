4f082c0992699b355503c62d66536ee56912dfae
taftadjani <taftadjani@gmail.com>
2022-03-14 15:11:56 +0100

    Create Storage Constants - v3
    - [x] t6 - Add profile variables
    	- [x] *firstname*
    	- [x] *lastname*
    	- [x] *email*
    	- [x] *avatar*

--- lib/constants/storage_constant.dart
+++ lib/constants/storage_constant.dart
@@ -1,6 +1,12 @@
-class StorageConstant{
+class StorageConstant {
   static const String isDarkMode = 'isDarkMode';
-  
+
   // First time user use the app. Used to show or not the onboarding screen
   static const String isFirstEntrance = 'isFirstEntrance';
+
+  // Profile
+  static const String firstname = 'firstname';
+  static const String lastname = 'lastname';
+  static const String email = 'email';
+  static const String avatar = 'avatar';
 }
